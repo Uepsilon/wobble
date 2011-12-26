d56d4970d500aceb5946792c38dec537425f7e5d
Stephan Zeissler <stephan.zeissler@moinz.de>
2011-12-26 16:15:47 +0100

    Made the validationservice more php5ish

--- web/api/classes/ValidationService.class.php
+++ web/api/classes/ValidationService.class.php
@@ -1,13 +1,19 @@
 <?php
 class ValidationService {
-	function validate_email($input) {
+	public static function validate_email($input) {
 		ValidationService::check(!empty($input) && strpos($input, '@') > 0, 'Valid email adress required: ' . $input);
 	}
-	function validate_not_empty($input) {
+	public static function validate_not_empty($input) {
 		ValidationService::check(!empty($input));
 	}
+
+	public static function validate_boolean($input) {
+		ValidationService::check ($input === true || $input === false || $input === 1 || $input === 0, 'Provide either true or false.');
+	}
 	
-	function check($boolean, $message = 'Invalid Input!') {
+	public static function check($boolean, $message = 'Invalid Input!') {
 		if ( !$boolean ) throw new Exception($message);
 	}
+
+	
 }
